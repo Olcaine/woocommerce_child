bb29eeb1f0c42f03e5fca25eca80f3e216e0cf29
Caroline <c.bove16@gmail.com>
2018-10-30 14:50:53 +0100

    get_price() see differents messages for differents prices

--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -6,5 +6,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product;
 
-?>
-<p class="price">prix : <?php echo $product->get_price_html(); ?></p>
+if ($product->get_price() < 20){ ?>
+    <p class="price"> promotions : <?php echo $product->get_price_html(); ?></p> <?php
+} else {
+    ?> <p class="price"> prix : <?php echo $product->get_price_html(); ?></p> <?php
+}?>
